1aab11acdff1e7cacf3731f3eeefb2d131d913d9
Andrew S <37746576+asalvia@users.noreply.github.com>
2018-10-03 15:11:37 -0400

    Add files via upload

---
+++ Parallax Site/script/script.js
@@ -0,0 +1,8 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+
+$('#myDropdown').on('show.bs.dropdown', function () {
+    $('.dropdown-toggle').dropdown()
+  })
+
+
